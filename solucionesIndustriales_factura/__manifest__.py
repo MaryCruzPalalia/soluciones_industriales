194955b5ecb2be0f8509e0ade66cbc24df838398
MaryCruzPalalia <mpm@soluciones4g.com>
2019-07-23 16:27:59 -0500

    [ADD]módulo de factura, se quito la fields de termino de pago

---
+++ solucionesIndustriales_factura/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name' : "quitar termino de pago en reporte factura",
+    'version' : "1.0",
+    'description' : "FACTURA ",
+    'author' : "Soluciones4g", 
+    'depends' : ['account',],
+    'data': [
+    	'views/factura_fields_mov.xml',
+    	        
+    ],
+    'installable' : True,
+}
